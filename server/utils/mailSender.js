9b7f8d566d13f75a12316a17edead85937fa414c
Tanuj Ganthade <tanujganthade43@gmail.com>
2026-07-25 18:34:22 +0530

    Add SMTP debug logs

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -12,6 +12,10 @@ const mailSender = async (email,title, body) => {
 
         await transporter.verify();
 console.log("SMTP Connected");
+console.log(process.env.MAIL_HOST);
+console.log(process.env.MAIL_USER);
+console.log(process.env.MAIL_PASS ? "PASS FOUND" : "PASS MISSING");
+
 
         let info = await transporter.sendMail({
             from: `"StudyNotion" <${process.env.MAIL_USER}>`,
@@ -25,7 +29,8 @@ console.log("SMTP Connected");
     }
 
     catch(error){
-        console.log(error);
+            console.error("Mail Error:", error);
+    throw error;
     }
 }
 
