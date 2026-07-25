176d75bd4339e58bd6758182321b0f6d0a4fee94
Tanuj Ganthade <tanujganthade43@gmail.com>
2026-07-25 18:06:47 +0530

    Fix Gmail transporter

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -2,20 +2,16 @@ const nodemailer = require("nodemailer");
 
 const mailSender = async (email,title, body) => {
     try{
-    let transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: 587,
-      secure: false,
-      auth: {
-        //jisse email jayega woh mail user
-        user: process.env.MAIL_USER,
-        //password for sending mail
-        pass: process.env.MAIL_PASS,
-      },
-    });
+        let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS,
+        },
+        });
 
         let info = await transporter.sendMail({
-            from:'StudyNotion App- Tanuj',
+            from: `"StudyNotion" <${process.env.MAIL_USER}>`,
             to:`${email}`,
             subject:`${title}`,
             html:`${body}`
