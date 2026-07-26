205ac0821c2b58e9bcda37b0f31a506eb493c590
Tanuj Ganthade <tanujganthade43@gmail.com>
2026-07-26 11:11:27 +0530

    Force IPv4 for Nodemailer

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,12 +1,31 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   auth: {
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASS,
+//   },
+// });
+
+
+
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
